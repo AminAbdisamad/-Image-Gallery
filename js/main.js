49a69ae773564efe93697279c2ce5f0e8612be80
Amin Abdisamad <theaminux@gmail.com>
2018-01-22 18:39:45 +0300

    Modified js/main.js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// creating current image
+//const current = document.querySelector("#current");
+// creating images
+//const imgs = document.querySelectorAll(".thum-img img");
+// looping through images using forEach
+const [current,imgs]=[document.querySelector("#current"),document.querySelectorAll(".thum-img img")];
+const opacity = 0.2;
+imgs.forEach(function(img){
+    img.addEventListener('click',imgClick);
+});
+
+function imgClick(e){
+    //Reset Opacity
+    imgs.forEach(img => (img.style.opacity=1));
+    // changing the current image to a source of clicked image
+    current.src = e.target.src;
+    // change the opacity of image to the opacity var
+    e.target.style.opacity = opacity;
+}
+
+/** the short way 
+imgs.forEach(img => img.addEventListener('click',(e)=> 
+current.src = e.target.src));
+*/
